cf15e1cae694dc2fecc62405f3eecb9892b10f69
pieter vermeulen <pieter.vermeulen@spinewise.be>
2025-12-23 15:33:04 +0100

    muziek

--- src/components/AudioPlayer.tsx
+++ src/components/AudioPlayer.tsx
@@ -25,8 +25,8 @@ const AudioPlayer = ({ pageType, audioFile }: AudioPlayerProps = {}) => {
       case 'openjegeest': return '/Open_Je_Geest_1.mp3';
       case 'leven': return '/leven2.mp3';
       case 'weeszot': return '/Wees_Zot.mp3';
-      case 'geld': return '/btc.mp3';
-      case 'ziekzot': return '/De_natuur_geneest.mp3';
+      case 'geld': return '/btc2.mp3';
+      case 'ziekzot': return '/sun.mp3';
       default: return '/startpagina2.mp3';
     }
   };
